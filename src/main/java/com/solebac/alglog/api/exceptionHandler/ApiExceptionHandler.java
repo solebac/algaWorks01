3ec116d5a5bdcab76fdceea80924a5e71186be3e
Flavio Rogerio <solebac@hotmail.com>
2022-07-21 09:58:05 -0300

    Implementando Exception Handler

---
+++ src/main/java/com/solebac/alglog/api/exceptionHandler/ApiExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.solebac.alglog.api.exceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		
+		
+		Problema obj = new Problema();
+		obj.setStatus(status.value());
+		obj.setDataHora(LocalDateTime.now());
+		obj.setTitulo("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.");
+		
+		return handleExceptionInternal(ex, obj, headers, status, request);
+	}
+}
---
+++ src/main/java/com/solebac/alglog/api/exceptionHandler/Problema.java
@@ -0,0 +1,55 @@
+package com.solebac.alglog.api.exceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Problema {
+	private Integer status;
+	private LocalDateTime dataHora;
+	private String titulo;
+	private List<Campo> campos;
+	
+	public static class Campo{
+		private String nome;
+		private String mensagem;
+		public Campo(String nome, String mensagem) {
+			super();
+			this.nome = nome;
+			this.mensagem = mensagem;
+		}
+		
+	}
+	
+	public List<Campo> getCampos() {
+		return campos;
+	}
+
+	public void setCampos(List<Campo> campos) {
+		this.campos = campos;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+}
