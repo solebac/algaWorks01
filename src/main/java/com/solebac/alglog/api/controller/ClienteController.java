88a4e219d2340c32cc0a1adf53300adedf028bc4
Flavio Rogerio <solebac@hotmail.com>
2022-07-21 01:56:39 -0300

    Postman return 200ok

--- src/main/java/com/solebac/alglog/api/controller/ClienteController.java
+++ src/main/java/com/solebac/alglog/api/controller/ClienteController.java
@@ -1,16 +1,14 @@
 package com.solebac.alglog.api.controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.solebac.alglog.api.controller.domain.model.Cliente;
@@ -35,9 +33,12 @@ public class ClienteController {
 	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		return clienteRepository.findById(clienteId)
-				.map(client -> ResponseEntity.ok(client))
+				.map(ResponseEntity::ok)//.map(client -> ResponseEntity.ok(client))
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	
+	@PostMapping
+	public Cliente adicionar(@RequestBody Cliente client){
+		return clienteRepository.save(client);
+	}
 }
