a3372a86826302e15c2d1ec696098d56635872ea
Flavio Rogerio <solebac@hotmail.com>
2022-07-21 01:48:03 -0300

    Method GetMapping for Id

--- src/main/java/com/solebac/alglog/api/controller/ClienteController.java
+++ src/main/java/com/solebac/alglog/api/controller/ClienteController.java
@@ -1,18 +1,23 @@
 package com.solebac.alglog.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.solebac.alglog.api.controller.domain.model.Cliente;
 import com.solebac.alglog.api.controller.domain.model.repositories.ClienteRepository;
 
 @RestController
+@RequestMapping(value = "/clientes")
 public class ClienteController {
 	
 	private ClienteRepository clienteRepository;
@@ -22,11 +27,20 @@ public class ClienteController {
 		this.clienteRepository = clienteRepository;
 	}
 
-
-
-	@GetMapping(value = "/clientes")
+	@GetMapping
 	public List<Cliente> lista() {
-		//Codigo JPQL
 		return clienteRepository.findAll();
 	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> obj = clienteRepository.findById(clienteId);
+		//return obj.orElse(null); sempre retorno 200ok|Isso é ruim
+		if (obj.isPresent()) {
+			return ResponseEntity.ok(obj.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	
 }
--- src/main/java/com/solebac/alglog/api/controller/domain/model/repositories/ClienteRepository.java
+++ src/main/java/com/solebac/alglog/api/controller/domain/model/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.solebac.alglog.api.controller.domain.model.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.solebac.alglog.api.controller.domain.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+	
 }
