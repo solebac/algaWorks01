55a7fd3e8a926b8eb4169c8166689f9c95096242
Flavio Rogerio <solebac@hotmail.com>
2022-07-21 02:03:34 -0300

    Postman return 201ok|Implement anotação @ResponseStatus

--- src/main/java/com/solebac/alglog/api/controller/ClienteController.java
+++ src/main/java/com/solebac/alglog/api/controller/ClienteController.java
@@ -2,13 +2,16 @@ package com.solebac.alglog.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.solebac.alglog.api.controller.domain.model.Cliente;
@@ -38,7 +41,10 @@ public class ClienteController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente client){
 		return clienteRepository.save(client);
 	}
+	
+	
 }
