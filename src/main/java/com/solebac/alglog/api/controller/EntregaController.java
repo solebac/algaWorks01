51a922aa2a73a972f19706b1684044b9524a41af
Flavio Rogerio <solebac@hotmail.com>
2022-07-22 01:25:45 -0300

    Alter Service SolicitacaoEntregaService for Exception NegocioException

---
+++ src/main/java/com/solebac/alglog/api/controller/EntregaController.java
@@ -0,0 +1,29 @@
+package com.solebac.alglog.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.solebac.alglog.api.controller.domain.model.Entrega;
+import com.solebac.alglog.api.controller.domain.services.SolicitacaoEntregaService;
+
+@RestController
+@RequestMapping("/entregas")
+public class EntregaController {
+
+	private SolicitacaoEntregaService entregaservice;
+
+	public EntregaController(SolicitacaoEntregaService entregaservice) {
+		super();
+		this.entregaservice = entregaservice;
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Entrega solicitar(@RequestBody Entrega entrega) {
+		return entregaservice.solicitar(entrega);
+	}
+}
--- src/main/java/com/solebac/alglog/api/controller/domain/services/SolicitacaoEntregaService.java
+++ src/main/java/com/solebac/alglog/api/controller/domain/services/SolicitacaoEntregaService.java
@@ -6,20 +6,33 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.solebac.alglog.api.controller.domain.exception.NegocioException;
+import com.solebac.alglog.api.controller.domain.model.Cliente;
 import com.solebac.alglog.api.controller.domain.model.Entrega;
 import com.solebac.alglog.api.controller.domain.model.StatusEntrega;
+import com.solebac.alglog.api.controller.domain.model.repositories.ClienteRepository;
 import com.solebac.alglog.api.controller.domain.model.repositories.EntregaRepository;
 
 @Service
 public class SolicitacaoEntregaService {
 	private EntregaRepository entregaRepository;
+	private ClienteRepository clienteRepository;
 
-	public SolicitacaoEntregaService(EntregaRepository entregaRepository) {
+	public SolicitacaoEntregaService(EntregaRepository entregaRepository, ClienteRepository clienteRepository) {
+		super();
 		this.entregaRepository = entregaRepository;
+		this.clienteRepository = clienteRepository;
 	}
-	
+
+
+
 	@Transactional
 	public Entrega solicitar(Entrega entrega) {
+		
+		Cliente cliente = clienteRepository.findById(entrega.getCliente().getId())
+				.orElseThrow(() -> new NegocioException("Cliente not found."));
+		
+		entrega.setCliente(cliente);
 		entrega.setStatus(StatusEntrega.PENDENTE);
 		entrega.setDataPedido(LocalDateTime.now());
 		//Implements regra de horario
